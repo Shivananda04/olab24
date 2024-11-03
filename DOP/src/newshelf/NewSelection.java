f30d9a76a6b0ba277dfb2858e1e3df45914b9d49
Shivananda04 <shivananda5005@gmail.com>
2024-11-03 21:39:30 +0530

    Update NewSelection.java

--- DOP/src/newshelf/NewSelection.java
+++ DOP/src/newshelf/NewSelection.java
@@ -1,5 +1,6 @@
-package newshelf;
+package Bonus;
 
+import java.util.*;
 
 public class NewSelection {
 
@@ -12,16 +13,46 @@ public class NewSelection {
 	 */
 	public static String getAgeOrTitle(Object o) {
 		
-		if (o instanceof Book) {
-			
+		// if (o instanceof Comic c) {
+		// 	return ((Comic) o).Title();
+		// }
+		// else if(o instanceof TextBook t){
+		// 	return ((TextBook) o).subject();
+		// }	
+		// else if(o instanceof Fiction f){
+		// 	return ((Fiction) o).name();
+		// }
+		switch(o){
+			case Comic(String Title,int ageOfMainCharacter) -> {
+				return Title;
+			}
+			case Fiction(String name,FictionType type) -> {
+				return name;
+			}
+			case TextBook(String subject) -> {
+				return subject;
+			}
+			default -> {
+				return null;
+			}
 		}
-		return null;
+		
+
 	}
 
 	public static void main(String[] args) {
 		
 		// TODO: Write a test code here and execute and text.
-		TextBook t = new TextBook();
-		
+		IBook<Integer> c1 = new Comic("Superhero Adventures", 12);
+        IBook<Integer> c2 = new Comic("Mystery Island", 15);
+        IBook<FictionType> f1 = new Fiction("Romeo and Juliet", FictionType.Tragedy);
+        IBook<FictionType> f2 = new Fiction("The Comedy of Errors", FictionType.Comedy);
+        IBook<String> t1 = new TextBook("Mathematics");
+        IBook<String> t2 = new TextBook("Physics");
+
+		System.out.println("\nTesting compareTo method:");
+        System.out.println("c1 vs c2: " + c1.compareTo(c2)); 
+        System.out.println("f1 vs f2: " + f1.compareTo(f2));
+        System.out.println("t1 vs t2: " + t1.compareTo(t2));
 	}
 }
